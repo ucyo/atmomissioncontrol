d4a9b1fec1a38a6ee20ea9718c1bb54d6b742b03
ucyo <cayoglu@me.com>
2019-04-20 08:07:06 +0200

    [feature] first running map

---
+++ static/map.js
@@ -0,0 +1,26 @@
+// window.onload = function () {
+//     var mymap = L.map('map').setView([51.505, -0.09], 13);
+
+//     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//         maxZoom: 18,
+//         id: 'mapbox.streets',
+//         accessToken: 'pk.eyJ1IjoidWN5byIsImEiOiJjaXNscG1oYTAwMDc5Mm5uNTBudDhoNm5uIn0.ofb4dzyt3bY9h6Bqul_Amg'
+//     }).addTo(mymap);
+
+// };
+
+window.onload = function () {
+    var mymap = L.map('map').setView([-25.505, -63.09], 3);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+    // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    //     maxZoom: 18,
+    //     id: 'mapbox.dark',
+    //     accessToken: 'paste_token'
+    // }).addTo(mymap);
+
+};
